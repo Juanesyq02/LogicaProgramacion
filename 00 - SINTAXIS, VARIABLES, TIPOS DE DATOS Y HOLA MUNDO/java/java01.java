1271402f6fa87567ea59f7f095b1882c57aaae9b
Juanesyq02 <juaneess43@hotmail.es>
2025-02-12 16:45:07 -0500

    carpeta y ejercicio hecho en java

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/java01.java
@@ -0,0 +1,52 @@
+public class Juanesyq02 {
+
+
+    public static void main(String[] args) {
+
+
+    //https://www.java.com/es/
+
+    //Esto es un comentario
+
+    /*
+    Aquí dentro puede haber otro comentario
+
+    las personas tambien declaran las variables y luego les dan valores en otras lineas siguientes.
+
+    */
+
+    //EJERCICIO:
+    /*
+     -Crea un comentario en el código y coloca la URL del sitio web oficial del
+    -lenguaje de programación que has seleccionado.
+    -Representa las diferentes sintaxis que existen de crear comentarios
+    -en el lenguaje (en una línea, varias...).
+    -Crea una variable (y una constante si el lenguaje lo soporta).
+    -Crea variables representando todos los tipos de datos primitivos
+    -del lenguaje (cadenas de texto, enteros, booleanos...).
+    -Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+    -¿fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+    debemos comenzar por el principio.
+     */
+
+        //Constante
+        final int CONSTANTE_NUMERO = 12;
+
+    //variables
+
+        int numer0 = 12;
+        //hay que poner L
+        long numGrande = 321312312L;
+        //hay que poner f
+        float decimal = 12.3f;
+        double decimalGrande = 1.56;
+        short numChico = 10;
+        boolean check = true;
+        char carcter = 'a';
+        byte eso = 10;
+        String texto = "QUE CARALLO";
+
+        System.out.println("Hola,¡Java!");
+
+    }
+}
