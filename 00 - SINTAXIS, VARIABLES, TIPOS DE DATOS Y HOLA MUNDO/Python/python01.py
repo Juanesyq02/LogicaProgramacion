aa91cf9a12ade06bfd2a9614cdb7ccd0481343e0
Juanesyq02 <juaneess43@hotmail.es>
2025-02-12 16:14:05 -0500

    se agrega la carpeta del primer modulo de aprendizade de logica de programacion

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Python/python01.py
@@ -0,0 +1,52 @@
+# http://python.org
+
+# Comentario en una linea
+
+"""
+Esto es
+un comentario
+en varias lineas
+
+"""
+
+'''
+Esto tambien es
+un comentario 
+en varias lineas
+
+'''
+
+#Ejercicio:
+
+"""
+EJERCICIO:
+-Crea un comentario en el código y coloca la URL del sitio web oficial del
+-lenguaje de programación que has seleccionado.
+-Representa las diferentes sintaxis que existen de crear comentarios
+-en el lenguaje (en una línea, varias...).
+-Crea una variable (y una constante si el lenguaje lo soporta).
+-Crea variables representando todos los tipos de datos primitivos
+-del lenguaje (cadenas de texto, enteros, booleanos...).
+-Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+-¿fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+debemos comenzar por el principio.
+ 
+"""
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convencion
+
+my_int = 25
+my_float = 25.1
+my_bool = True
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
