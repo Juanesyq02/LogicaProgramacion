b897146c5c106ed7fb801f0667af937dbcdfd46f
Juanesyq02 <juaneess43@hotmail.es>
2025-02-12 16:32:20 -0500

    se agrega la carpeta y se desarrolla el ejercicio en javascript

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/javascript01.js
@@ -0,0 +1,29 @@
+// Sitio web oficial: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una linea
+
+/* Comentario
+en multiples
+lineas */
+
+//Ejercicio:
+/*
+-Crea un comentario en el código y coloca la URL del sitio web oficial del
+-lenguaje de programación que has seleccionado.
+-Representa las diferentes sintaxis que existen de crear comentarios
+-en el lenguaje (en una línea, varias...).
+-Crea una variable (y una constante si el lenguaje lo soporta).
+-Crea variables representando todos los tipos de datos primitivos
+-del lenguaje (cadenas de texto, enteros, booleanos...).
+-Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+-¿fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+debemos comenzar por el principio.
+*/
+
+var languageName = 'JavaScript';
+var stringType = 'string';
+var numberType = 10;
+var booleanType = true;
+var nullType = null;
+var undefinedType = undefined;
+console.log("Hola " + languageName + "!");
